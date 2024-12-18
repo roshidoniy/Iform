432e9d8208772273cd3ec3f797976dd3b9a5a0ec
roshidoniy <samadbek.mahmudjonov@gmail.com>
2024-12-18 11:16:41 +0500

    Login Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import react from "../assets/react.svg"
+import { Link } from "react-router"
 const Home = () => {
     return (
         <div className="w-full h-screen flex flex-col items-center justify-center">
-            {/* tailwind css class */}
             <img src={react} alt="react logo" className="w-20 h-20 animate-bounce" />
+
             <h1>Home</h1>
             <p>Home Page</p>
+            <Link to="/login">Login</Link>
         </div>
     )
 }
