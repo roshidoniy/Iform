5a368caa51afdbd80ec9c809064831dc640d4c75
roshidoniy <samadbek.mahmudjonov@gmail.com>
2024-12-18 11:19:44 +0500

    Login Page

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router'
@@ -6,9 +5,7 @@ import App from './App.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
 )
