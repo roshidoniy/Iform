09cc5a0b04661368df9ed7856559408a7b4fd667
roshidoniy <samadbek.mahmudjonov@gmail.com>
2025-02-24 18:15:19 +0500

    View Form Button was added in ManageForm.tsx

--- src/pages/templatePages/ManageForm.tsx
+++ src/pages/templatePages/ManageForm.tsx
@@ -108,6 +108,16 @@ const ManageForm = () => {
                 <p className="text-gray-600">Design your form by adding questions and customizing settings</p>
                 <div className="mt-4 flex justify-center gap-4">
                     <button
+                        onClick={() => navigate(`/template/${tid}/view`)}
+                        className="inline-flex items-center px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-all"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                            <path fillRule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd" />
+                        </svg>
+                        View Form
+                    </button>
+                    <button
                         onClick={() => navigate(`/template/${tid}/edit/results`)}
                         className="inline-flex items-center px-4 py-2 bg-gray-600 text-white rounded-lg hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition-all"
                     >
